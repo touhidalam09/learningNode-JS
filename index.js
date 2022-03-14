2e2c94fad5132bf3f5684f533b1b6b5b020aa318
touhidalam09 <touhidalam649@gmail.com>
2022-03-15 02:59:49 +0600

    parsing request

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 // Dependence
 const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
 
 // App Object - Module Scaffolding
 const app = {};
@@ -26,8 +28,27 @@ app.createServer = () => {
 
 // handle request Response
 app.handleReqRes = (req, res) => {
-  // response handle
-  res.end("Raw Node mon!!");
+  // request handle
+  // get the url with parse
+  const parseUrl = url.parse(req.url, true);
+  const pathName = parseUrl.pathname;
+  const trimmedPath = pathName.replace(/^\/+|\/+$/g, ""); //for route pathName
+  const queryStringObject = pathName.query; // for query string
+  const method = req.method.toLowerCase(); // for method request
+  const headerObject = req.headers; // for header metadata
+  //body start for POST method
+  const decode = new StringDecoder("utf-8"); // for body data
+  const realData = "";
+  req.on("data", (buffer) => {
+    realData += decode.write(buffer);
+  });
+  req.on("end", () => {
+    realData += decode.end();
+    console.log(method);
+    // response handle
+    res.end("Raw Node mon!!");
+  });
+  //body end
 };
 
 // start the server
